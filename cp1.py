58e8848d3ed507ed5442121acbcac9c6115d298f
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-04-24 18:24:34 +0530

    cp questions

--- cp1.py
+++ cp1.py
@@ -1,15 +1,15 @@
 #Hackerrank Q.Find the Runner-Up Score!
-if __name__ == '__main__':
+"""if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
 
 array = list(arr)
 winner = max(array)
 array = [i for i in array if i != winner]
-print(max(array))
+print(max(array))"""
 
 #Hackerrank Q.List Comprehensions
-if __name__ == '__main__':
+"""if __name__ == '__main__':
     x = int(input())
     y = int(input())
     z = int(input())
@@ -22,4 +22,19 @@ for i in range(x+1):
             if i+j+k != n:
                 l.append([i,j,k])
 
-print(l)
+print(l)"""
+
+#CodeChef Q
+T = int(input())
+for i in range(T):
+    x = input()
+    y = input()
+
+ans = False
+
+for j in len(x):
+    if j == j and j != '?' and j != '?':
+        ans = True
+        print(ans)
+
+print(ans)
