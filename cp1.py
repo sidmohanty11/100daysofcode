a9a625b8588a1d2caec6de0e91ed4b988d576da4
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-04-24 19:33:08 +0530

    trying some hackerrank questions

---
+++ cp1.py
@@ -0,0 +1,25 @@
+#Hackerrank Q.Find the Runner-Up Score!
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+array = list(arr)
+winner = max(array)
+array = [i for i in array if i != winner]
+print(max(array))
+
+#Hackerrank Q.List Comprehensions
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+l = list()
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k != n:
+                l.append([i,j,k])
+
+print(l)
