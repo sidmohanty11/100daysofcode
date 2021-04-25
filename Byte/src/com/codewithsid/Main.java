c5bd21443ac28d4ffcb97d8be0da0c8d3b39963f
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-04-25 12:12:23 +0530

    more learning

---
+++ Byte/src/com/codewithsid/Main.java
@@ -0,0 +1,16 @@
+package com.codewithsid;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        double x = 200.0235d;
+        double y= 220.2589d;
+        double z =91923.2323d;
+        double theSum= x+y+z;
+        boolean sum= (theSum!=0) ? true :false;
+        System.out.println("The sum is not zero and it is =" + theSum);
+        }
+    }
+}
