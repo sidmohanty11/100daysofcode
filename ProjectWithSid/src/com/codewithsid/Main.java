7a12a710cd45240c65c8e17e3cbbe8ca926d96d5
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-04-25 12:23:52 +0530

    learning java

---
+++ ProjectWithSid/src/com/codewithsid/Main.java
@@ -0,0 +1,17 @@
+package com.codewithsid;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("I am a disco dancer");
+        int a=4, b=5, c=6;
+        System.out.println(a+b+c);
+        String name= "Jinu";
+        Boolean Jinu= Boolean.TRUE;
+        System.out.println(name);
+        System.out.println(Jinu);
+
+
+    }
+}
